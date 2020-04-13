b9c562e913fbef323a00f28c144a25f9ec09b1bd
Ksychov <63414161+Ksychov@users.noreply.github.com>
2020-04-13 21:26:47 +0300

    Add files via upload

---
+++ Parking.java
@@ -0,0 +1,69 @@
+import java.util.Arrays;
+
+public class Parking {
+    String[] parking;
+    int size;
+
+    public Parking(int n) {
+        size = n;
+        parking = new String[size];
+        for (int i = 0; i < n; i++) {
+            parking[i] = "";
+        }
+    }
+
+    public void addCar(String number) {
+        int pos = firstFreePlace();
+        parking[pos] = number;
+    }
+
+    private int firstFreePlace() {
+        for (int i = 0; i < size; i++) {
+            if (parking[i].equals("")) { // !parking[i].isEmpty()
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    public void freeCar(String number) {
+        for (int i = 0; i < parking.length; i++) {
+
+            if (parking[i].equals(number)) {
+                parking[i] = "";
+            }
+        }
+
+    }
+
+
+    public void show() {
+        System.out.println(Arrays.toString(parking));
+    }
+
+    public int freePlaces() {
+        int howmany = 0;
+        for (int i = 0; i < size; i++) {
+            if (parking[i].equals("")) { // !parking[i].isEmpty()
+                howmany++;
+            }
+
+        }
+        return howmany;
+    }
+}
+
+class TestParking {
+    public static void main(String[] args) {
+        Parking p = new Parking(10);
+        p.addCar("a123bc");
+        p.show();
+
+        p.addCar("b321ef");
+        p.show();
+        System.out.println(p.freePlaces());
+
+        p.freeCar("a123bc");
+        p.show();
+    }
+}
